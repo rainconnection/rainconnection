768181661f6ea789ee6d412e4b7ef9290855e85c
hwan <k01088789552@gmail.com>
2024-06-06 19:04:08 +0900

    GDCN inital commit_2

--- Recommend_CTR/models/GDCN.py
+++ Recommend_CTR/models/GDCN.py
@@ -17,3 +17,25 @@ class GDCN(nn.Module):
         super(GDCN, self).__init__()
 
         self.embedding_vector =  EmbeddingDict(category_list, vector_dim)
+        self.GCLBlock = GatedCrossLayer()
+
+
+
+
+class GatedCrossLayer(nn.Module):
+    def __init__(self, input_dim : int, num_gc : int, gate_function = nn.Sigmoid()):
+        # input_dim : embedding_vector_dim * number of feature?
+        super(GatedCrossLayer, self).__init__()
+
+        self.num_gc = num_gc
+        self.gate_function = gate_function
+
+
+    def forward(self, x):
+        # cross networking
+        '''
+        c_(l+1) = c0*(wc_l + bias_l) * gated(wg_l) + c_l
+        
+        wc_l : nn.linear(c_l)
+        wg_l : nn.linear(c_l)
+        '''
