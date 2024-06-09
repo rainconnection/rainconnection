860ff8e4436f3e86a1e308e2830d7505b66cb76f
hwan <k01088789552@gmail.com>
2024-06-09 15:33:41 +0900

    GDCN layerblock commit

--- Recommend_CTR/models/GDCN.py
+++ Recommend_CTR/models/GDCN.py
@@ -12,7 +12,7 @@ from typing import List
 
 from embedding.feature_embedding import EmbeddingDict
 
-class GDCN(nn.Module):
+class GDCN(nn.Module): # 기본 구조. Stack, parallel로 세분화 필요. 2개로?
     def __init__(self, category_list : List[str], vector_dim : int = 2**7):
         super(GDCN, self).__init__()
 
@@ -22,14 +22,23 @@ class GDCN(nn.Module):
 
 
 
-class GatedCrossLayer(nn.Module):
-    def __init__(self, input_dim : int, num_gc : int, gate_function = nn.Sigmoid()):
+class GatedCrossLayerBlock(nn.Module):
+    def __init__(self, input_dim : int, num_layers : int, gate_function = nn.Sigmoid()):
         # input_dim : embedding_vector_dim * number of feature?
-        super(GatedCrossLayer, self).__init__()
-
-        self.num_gc = num_gc
+        super(GatedCrossLayerBlock, self).__init__()
+        input_dim = input_dim
+        self.num_layers = num_layers
         self.gate_function = gate_function
-
+        
+        self.wc = nn.ModuleList()
+        self.wg = nn.ModuleList()
+        self.bias = nn.ModuleList()
+        for _ in range(self.num_layers):
+            self.wc.append(nn.Linear(input_dim, input_dim))
+            self.wg.append(nn.Linear(input_dim, input_dim))
+            self.bias.append(
+                nn.Parameter(torch.zeroes((input_dim,)))
+                )
 
     def forward(self, x):
         # cross networking
@@ -39,3 +48,11 @@ class GatedCrossLayer(nn.Module):
         wc_l : nn.linear(c_l)
         wg_l : nn.linear(c_l)
         '''
+        x0 = x
+        for i in range(self.num_layers):
+            xc = self.wc[i](x)
+            xg = self.gate_function(self.wg[i](x))
+
+            x = x0*(xc+self.bias[i])*xg + x
+
+        return x # [input_dim]
